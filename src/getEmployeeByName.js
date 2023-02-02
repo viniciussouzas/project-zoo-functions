60c85f1f91a0f497fddeed860029b34525c3e0fa
Vinícius Souza <tradutorvinicius@gmail.com>
2023-02-02 12:43:12 -0300

    Resolve requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const getEmployee = data.employees.find((employee) => (employee.firstName === employeeName
+    || employee.lastName === employeeName));
+
+  return getEmployee;
 };
 
 module.exports = getEmployeeByName;
