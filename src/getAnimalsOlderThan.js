2d1c9c76c30b2fa84ac6042c160339c6151a97e7
Vinícius Souza <tradutorvinicius@gmail.com>
2023-02-02 12:20:33 -0300

    Resolve requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const getSpecie = data.species.find((specie) => specie.name === animal);
+
+  const verifyAge = getSpecie.residents.every((resident) => resident.age >= age);
+
+  return verifyAge;
 };
+
+console.log(getAnimalsOlderThan('lions', 7));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -18,6 +18,4 @@ const getSpeciesByIds = (...ids) => {
   }
 };
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '78460a91-f4da-4dea-a469-86fd2b8ccc84'));
-
 module.exports = getSpeciesByIds;
