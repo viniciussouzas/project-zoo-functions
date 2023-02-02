de15a5e93c420354074535367cbabe8bfb4e7d5d
Vinícius Souza <tradutorvinicius@gmail.com>
2023-02-02 17:00:37 -0300

    Retira console.log do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,22 +8,16 @@ const isManager = (id) => {
 };
 
 const getRelatedEmployees = (managerId) => {
-  try {
-    if (isManager(managerId)) {
-      const getEmployees = data.employees.filter((employee) =>
-        employee.managers.includes(managerId));
+  if (isManager(managerId)) {
+    const getEmployees = data.employees.filter((employee) =>
+      employee.managers.includes(managerId));
 
-      return getEmployees.map((employee) => `${employee.firstName} ${employee.lastName}`);
-    }
+    return getEmployees.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
 
-    if (!isManager(managerId)) {
-      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-    }
-  } catch (error) {
-    return 'O id inserido não é de uma pessoa colaboradora gerente!';
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 };
-
-console.log(getRelatedEmployees());
 
 module.exports = { isManager, getRelatedEmployees };
