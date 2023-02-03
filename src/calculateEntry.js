7a9cd0ac50f731f4a9fac8002dbefc4abbc88904
Vinícius Souza <tradutorvinicius@gmail.com>
2023-02-03 17:41:58 -0300

    Resolve requisito 9, com muito custo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -30,15 +30,4 @@ const calculateEntry = (entrants) => {
   return Number(getValue.toFixed(2));
 };
 
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
-console.log(countEntrants(entrants));
-
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+const scheduleDay = (day) => {
+  if (day !== 'Monday') {
+    return `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+  }
+
+  return 'CLOSED';
 };
+
+const scheduleSpecie = (day) => {
+  if (day === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+
+  const filteredSpecies = data.species.filter((specie) => specie.availability.includes(day));
+
+  const mappedSpecies = filteredSpecies.map((specie) => specie.name);
+
+  return mappedSpecies;
+};
+
+const getSchedule = (scheduleTarget) => {
+  const week = Object.keys(data.hours).some((key) => key === scheduleTarget);
+  if (week === true) {
+    return { [scheduleTarget]: {
+      officeHour: scheduleDay(scheduleTarget),
+      exhibition: scheduleSpecie(scheduleTarget),
+    } };
+  }
+  const getAnimals = data.species.some((specie) => specie.name === scheduleTarget);
+  const mappedDays = Object.keys(data.hours).map((day) => ({ [day]: {
+    officeHour: scheduleDay(day),
+    exhibition: scheduleSpecie(day),
+  },
+  }));
+  if (getAnimals === false || scheduleTarget === undefined) {
+    return Object.assign(...mappedDays);
+  }
+  const getAnimal = data.species.find((specie) => specie.name === scheduleTarget);
+  return getAnimal.availability;
+};
+
+console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
