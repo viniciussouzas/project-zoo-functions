2fbe0958f5acef72bc8f7748aa1593abacead6ed
Vinícius Souza <tradutorvinicius@gmail.com>
2023-02-06 14:55:44 -0300

    Resolve requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,64 @@
 const data = require('../data/zoo_data');
 
+const getAnimalByLoc = () => data.species.reduce((acc, specie) => {
+  if (acc[specie.location]) {
+    acc[specie.location].push(specie.name);
+  } else {
+    acc[specie.location] = [specie.name];
+  }
+  return acc;
+}, {});
+
+const getAnimalBySex = (object) => {
+  const getAnimalSex = data.species.reduce((acc, specie) => {
+    let result = specie.residents.filter((resident) =>
+      resident.sex === object.sex).map((animal) => animal.name);
+    if (object.sorted) {
+      result = specie.residents.filter((resident) =>
+        object.sex === resident.sex).map((animal) => animal.name).sort();
+    }
+    const animalSexNames = { [specie.name]: result };
+    if (acc[specie.location]) {
+      acc[specie.location].push(animalSexNames);
+    } else {
+      acc[specie.location] = [animalSexNames];
+    }
+
+    return acc;
+  }, {});
+
+  return getAnimalSex;
+};
+
+const getAnimalByName = (object) => {
+  const getAnimalName = data.species.reduce((acc, specie) => {
+    let result = specie.residents.map((resident) => resident.name);
+
+    if (object.includes('sorted')) {
+      result = specie.residents.map((animal) => animal.name).sort();
+    }
+
+    const animalNames = { [specie.name]: result };
+
+    if (acc[specie.location]) {
+      acc[specie.location].push(animalNames);
+    } else {
+      acc[specie.location] = [animalNames];
+    }
+    return acc;
+  }, {});
+
+  return getAnimalName;
+};
+
 const getAnimalMap = (options) => {
-  // seu código aqui
+  if (options === undefined || options.includeNames === undefined) return getAnimalByLoc();
+
+  if (options.sex !== undefined) return getAnimalBySex(options);
+
+  const optionsArray = Object.keys(options);
+
+  return getAnimalByName(optionsArray);
 };
 
 module.exports = getAnimalMap;
